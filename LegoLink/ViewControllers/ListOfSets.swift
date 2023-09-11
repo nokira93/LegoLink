a51e93146c85a26d506f1558107b80da4d5b2066
Artur <ajurkowski93@wp.pl>
2023-09-11 21:43:32 +0200

    no message

--- LegoLink/ViewControllers/ListOfSets.swift
+++ LegoLink/ViewControllers/ListOfSets.swift
@@ -163,6 +163,7 @@ extension ListOfSets {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SetsCell.identifier, for: indexPath) as? SetsCell else {
             fatalError("Error")
         }
+        print("Test \(indexPath.row) || \(modDataSource?.count)")
         cell.setUpView(url: modDataSource?[indexPath.row].setURL ?? "https://rebrickable.com/home/", name: modDataSource?[indexPath.row].name ?? "-", numOfParts: modDataSource?[indexPath.row].numParts ?? 0, imageURL: modDataSource?[indexPath.row].setImageURL ?? "-", setNum: modDataSource?[indexPath.row].setNum ?? "-")
         cell.backgroundColor = .clear
 //        let myCell = UITableViewCell(style: .subtitle , reuseIdentifier: nil)
